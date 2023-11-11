fae09e2b057d20d39172da9a1c6194a111b01fe2
Mitchell Alderson <mitch@farawaysouthwest.com>
2023-11-10 23:13:40 -0700

    add stream title

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,34 +1,35 @@
 import axios from "axios";
 
 export type VOD = {
-    streamName:      string;
-    vodName:         string;
-    streamId:        string;
-    creationDate:    number;
-    startTime:       number;
-    duration:        number;
-    fileSize:        number;
-    filePath:        string;
-    vodId:           string;
-    type:            string;
-    previewFilePath: null;
-}
+  streamName: string;
+  vodName: string;
+  streamId: string;
+  creationDate: number;
+  startTime: number;
+  duration: number;
+  fileSize: number;
+  filePath: string;
+  vodId: string;
+  type: string;
+  previewFilePath: null;
+};
 
 export default class http {
-    private static client = axios.create({
-        baseURL: `${import.meta.env.VITE_SERVER_URL}/LiveApp/rest/v2`,
-        headers: {
-            "Content-Type": "application/json",
-        },
+  private static client = axios.create({
+    baseURL: `${import.meta.env.VITE_SERVER_URL}`,
+  });
+
+  public static async getVODs(
+    page: number = 0,
+    size: number = 100,
+  ): Promise<VOD[]> {
+    const pageString = page.toString();
+    const sizeString = size.toString();
+
+    const response = await this.client.post<VOD[]>("/media/all", {
+      page: pageString,
+      size: sizeString,
     });
-
-    public static async getVODs(page: number = 0, size: number = 100): Promise<VOD[]> {
-        const response = await this.client.get<VOD[]>(`/vods/list/${page}/${size}`);
-        return response.data;
-    }
-
-    public static async getVOD(vodId: string): Promise<VOD> {
-        const response = await this.client.get<VOD>(`/vods/${vodId}`);
-        return response.data;
-    }
+    return response.data;
+  }
 }
